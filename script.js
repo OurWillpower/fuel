9b6e2e771e07a5cc1c87394f1b664307b7076861
Willpower Group <ourwillpower@gmail.com>
2026-05-25 18:04:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
+async function loadSection(id,file){
 
+try{
+
+const response=await fetch(file);
+
+const html=await response.text();
+
+document.getElementById(id).innerHTML=html;
+
+}
+
+catch(error){
+
+console.log("Error loading:",file);
+
+}
+
+}
+
+loadSection(
+"heroSection",
+"sections/hero.html"
+);
+
+loadSection(
+"aboutSection",
+"sections/about.html"
+);
+
+loadSection(
+"howSection",
+"sections/howitworks.html"
+);
+
+loadSection(
+"foundersSection",
+"sections/founders.html"
+);
+
+loadSection(
+"trustSection",
+"sections/trust.html"
+);
+
+loadSection(
+"contactSection",
+"sections/contact.html"
+);
+
+loadSection(
+"footerSection",
+"sections/footer.html"
+);
